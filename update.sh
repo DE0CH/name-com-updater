2abf94ae3d32434dd4bb37c9af0785a9b0a61d1d
Deyao Chen <chendeyao000@gmail.com>
2023-12-26 11:47:14 +0800

    fix bugs

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 export $(cat .env | xargs)
 PUBLIC_IP=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
-if [ ! -f last-ip.txt ] || [ $(cat last-ip.txt) != "$PUBLIC_IP" ] ; then
-    curl -u "$USERNAME:$TOKEN" "https://api.name.com/v4/domains/$DOMAIN_NAME/records/$RECORD_ID" -X PUT -H 'Content-Type: application/json' --data '{"host":"'"$RECORD_HOST"'","type":"A","answer":"'"$PUBLIC_IP"'","ttl":300}'
-    echo "$PUBLIC_IP" > last-ip.txt
-fi
+curl -u "$USERNAME:$TOKEN" "https://api.name.com/v4/domains/$DOMAIN_NAME/records/$RECORD_ID" -X PUT -H 'Content-Type: application/json' --data '{"host":"'"$RECORD_HOST"'","type":"A","answer":"'"$PUBLIC_IP"'","ttl":300}'
